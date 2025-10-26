366e842adbcf0819628794277b8c12f539300858
butros <butrosisaid@gmail.com>
2025-10-26 19:00:35 +0300

    test today

--- Program.cs
+++ Program.cs
@@ -5,3 +5,18 @@ Console.WriteLine("input one ");
 var input_2 = Console.ReadLine();
 Console.WriteLine($"input second ");
 
+
+Console.WriteLine("testc changes");
+Console.WriteLine("testc changes");
+
+Console.WriteLine("testc changes");
+
+
+var input_2 = Console.ReadLine();
+Console.WriteLine($"input second ");
+
+
+Console.WriteLine("testc changes");
+Console.WriteLine("testc changes");
+
+Console.WriteLine("testc changes");
